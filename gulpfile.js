175f5eb264c0c5953a7abfef910955b7120383e4
YuraSadilenko <yura.sadilenko.it@gmail.com>
2018-12-13 21:04:19 +0200

    start project

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var server = require('gulp-server-livereload');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  gulp.src('scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('autoprefix', function() {
+    return gulp.src("css/main.css")
+    .pipe(autoprefixer({
+        browsers: ['last 20 version'],
+        cascade: true
+    }))
+});
+
+gulp.task('server', function() {
+  gulp.src('')
+    .pipe(server({
+      livereload: true,
+      defaultFile: "index.html",
+      directoryListing: false,
+      open: true
+    }));
+  });
